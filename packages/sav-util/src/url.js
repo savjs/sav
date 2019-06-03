4e05a2f339a9d4e25c57bea4bb7e0b2ee90b5867
jetiny <86287344@qq.com>
2019-06-03 17:42:21 +0800

    remove some text

--- packages/sav-util/src/url.js
+++ packages/sav-util/src/url.js
@@ -1,9 +1,3 @@
-/*
- * @Description      URL解析
- * @File             url.js
- * @Auth             jetiny@hfjy.com
- */
-
 // jsuri https://code.google.com/r/jonhwendell-jsuri/
 // https://username:password@www.test.com:8080/path/index.html?this=that&some=thing#content
 const REKeys = ['source', 'protocol', 'authority', 'userInfo', 'user', 'password', 'host', 'port', 'relative', 'path', 'directory', 'file', 'query', 'anchor']
